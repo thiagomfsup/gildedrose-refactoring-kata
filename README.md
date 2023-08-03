b61937e4743296079a7f78a8edeac4a58048e3d5
thiagomfsup <thiagomfsup@gmail.com>
2023-08-03 17:25:53 +0200

    refactor updateQuality method

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This Kata was originally created by Terry Hughes (http://twitter.com/TerryHughes
 
 This Java code has been taken from Emily Bache's [GildedRose-Refactoring-Kata](https://github.com/emilybache/GildedRose-Refactoring-Kata/tree/main) repo.
 
-Gilded Rose Requirements Specification [here](https://github.com/thiagomfsup/gildedrose-refactoring-kata/GildedRoseRequirements.txt).
+Gilded Rose Requirements Specification [here](GildedRoseRequirements.txt).
 
 ## Solving Process
 ### 1st Iteration
@@ -13,3 +13,22 @@ Gilded Rose Requirements Specification [here](https://github.com/thiagomfsup/gil
 Unit tests has been written to give confidence during refactoring. While unit tests was being created, some observation has been made:
 1. The `GildedRose.updateQuality()` method not only updates `quality` field on an item, but also `sellIn` field, thus it violates the single responsibility principle (SRP).
 2. Item could somehow be classified into "Normal", "Aged", "Legendary", and "Backstage Pass" categories (eventually "Conjured" category shall be supported).
+
+### 2nd Iteration
+**Goals**:: refactoring the `GildedRose.updateQuality()` method.
+
+Since it is 100% covered by unit tests, it is time to "refactor" it!
+* Even though code coverage was 100%, there were corner cases that wasn't tested, which make me doubt about test coverage.
+
+#### First refactoring round:
+* use *enhanced for* statement instead;
+* give duplicated code a concept name. E.g.,
+```java
+  // Instead of :
+  if (items[i].quality < 50) {
+    items[i].quality = items[i].quality + 1;
+  }
+  // Use :
+  tryIncreaseItemQuality(Item item)
+```
+Ideal would be `item.tryIncreaseQuality()`, but rules said that `Item` class cannot be changed.
--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -8,55 +8,54 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
+        for (Item item : items) {
+            if (item.name.equals("Sulfuras, Hand of Ragnaros")) // LEGENDARY ITEM :: never has to be sold or decreases in Quality
+                continue;
+
+            if (!item.name.equals("Aged Brie") && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                tryDecreaseItemQuality(item);
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                // AGED or BACKSTAGE ITEM
+                tryIncreaseItemQuality(item);
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (item.sellIn < 11) {
+                        tryIncreaseItemQuality(item);
+                    }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+                    if (item.sellIn < 6) {
+                        tryIncreaseItemQuality(item);
                     }
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+            item.sellIn = item.sellIn - 1;
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
+            if (item.sellIn < 0) {
+                if (!item.name.equals("Aged Brie")) {
+                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        tryDecreaseItemQuality(item);
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        item.quality = 0;
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+                    tryIncreaseItemQuality(item);
                 }
             }
+        }
+    }
+
+    // TODO should be an item method
+    private void tryDecreaseItemQuality(Item item) {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
+        }
+    }
+
+    // TODO should be an item method
+    private void tryIncreaseItemQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
         }
     }
 }
